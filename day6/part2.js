ebfa9d5ad4d870830d2fcb895df94b25764de7dd
Justin Bezanson <justinbezanson@gmail.com>
2023-12-16 20:27:01 +0000

    day 6 part 2

--- day6/part2.js
+++ day6/part2.js
@@ -1,15 +1,33 @@
 "use strict";
 
-//https://adventofcode.com/2023/day/6
+//https://adventofcode.com/2023/day/6#part2
 
 /**
  * instance of a boat race
  */
 class Race {
-    constructor() {
+    /**
+     * 
+     * @param {string[]} lines 
+     */
+    constructor(lines) {
         this.duration = 0;
         this.distance = 0;
         this.winningRounds = 0;
+        this.init(lines);
+    }
+
+    /**
+     * initialize class values
+     * 
+     * @param {string[]} lines 
+     */
+    init(lines) {
+        const timeParts = lines[0].split(':')[1].trim().split(/\s+/);
+        const distanceParts = lines[1].split(':')[1].trim().split(/\s+/);
+
+        this.setDuration(parseInt(timeParts.join('')));
+        this.setDistance(parseInt(distanceParts.join('')));
     }
 
     /**
@@ -69,40 +87,19 @@ class Race {
      * create races array from lines
      * 
      * @param {string[]} lines 
-     * @returns {Race[]}
+     * @returns {Race}
      */
-    static CreateRaces(lines) {
-        /** @type {Race[]} */
-        let races = [];
-
-        lines.forEach((line) => {
-            /** @type {number[]} */
-            const parts = line.split(':')[1].trim().split(/\s+/).map(part => parseInt(part));
-
-            parts.forEach((part, index) => {
-                if(races[index] === undefined) {
-                    let race = new Race();
-                    race.setDuration(part);
-                    races[index] = race;
-                } else {
-                    races[index].setDistance(part);
-                }
-            });
-        });
-
-        return races;
+    static CreateRace(lines) {
+        const race = new Race(lines);    
+        return race;
     }
 }
 
 /** @type {string} */
 const input = require('./input.js');
 const lines = input.split('\n');
-const races = Race.CreateRaces(lines);
-let marginOfError = 1;
+const race = Race.CreateRace(lines);
 
-races.forEach((race) => {
-    race.compete();
-    marginOfError *= race.getWinningRounds();
-})
+race.compete();
 
-console.log(marginOfError); //answer: 1624896
+console.log(race.getWinningRounds()); //answer: 32583852
